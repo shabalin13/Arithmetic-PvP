c52121e8a67e22eb8f5852057959be0a6a3e5879
Aidar <khuzin.ja@yandex.ru>
2021-09-07 21:19:16 +0300

    New packages installed

--- Arithmetic_PvP_backend/Arithmetic_PvP_backend/settings.py
+++ Arithmetic_PvP_backend/Arithmetic_PvP_backend/settings.py
@@ -139,3 +139,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
--- Arithmetic_PvP_backend/Arithmetic_PvP_backend/urls.py
+++ Arithmetic_PvP_backend/Arithmetic_PvP_backend/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
