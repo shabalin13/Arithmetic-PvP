4658ba311c0af54947bbe75b8eebbe2b40cf162b
Aidar <khuzin.ja@yandex.ru>
2021-09-30 03:15:11 +0300

    Now player is created along with user activation

--- Arithmetic_PvP_backend/authentication/urls.py
+++ Arithmetic_PvP_backend/authentication/urls.py
@@ -5,7 +5,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     # path('google/', views.GoogleLogin.as_view(), name='google_login'),
-    path('<str:uid>/<str:token>', views.ActivateUser)
+    path('<str:uid>/<str:token>', views.activate_user)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
