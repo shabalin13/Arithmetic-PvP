910cf3a52d08fe10203797d562ba3ce0d0b78185
kamilain1 <69854194+kamilain1@users.noreply.github.com>
2021-10-05 13:43:25 +0300

    Added Activation page

---
+++ frontend/src/components/Activation.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="wrapper">
+        <div class="m-5 p-5 text-center">
+            <h1 class="my-5">Congrats, you have successfully activated your account via Email!</h1>
+            <button class="btn btn-lg btn-dark center" type="submit">Go to Main Page</button>
+        </div>
+    </div>
+</template>
+
+<script>
+     export default {
+        name: "Activation",
+        data() {
+        },
+        created() {
+        },
+        methods: {},}
+</script>
+
+<style scoped>
+
+</style>
